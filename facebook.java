40c280c9ebfb6d63aaa14a5ecac6196d4a186c5a
Bhoomikadk16 <bhoomikadk67@gmail.com>
2024-10-31 10:05:07 +0530

    java

---
+++ facebook.java
@@ -0,0 +1,12 @@
+public class facebook{
+    public static void main(String[] args){
+        signup();
+        login();
+    }
+    static void signup(){
+        System.out.println(" signup with facebook");
+    }
+    static void login(){
+        System.out.println("facebook login");
+    }
+}
